83777bcdaf6bfcd164d457ddcc7ec2987155973c
vinh.ntuan <vinh.ntuan@gmail.com>
2026-03-11 11:47:28 +0700

    First functional version

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name         YouTube Dual Real Subtitles
-// @namespace    yt.dualsub.real
+// @name         YouTube Dual Subtitles
+// @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Show two real YouTube subtitle tracks (EN + RU)
+// @description  Add dual subtitles to YouTube
+// @author       Vinh Nguyen
 // @match        https://www.youtube.com/watch*
 // @grant        none
 // ==/UserScript==
@@ -10,12 +11,10 @@
 (function () {
     "use strict";
 
-    const LANG1 = "en";
-    const LANG2 = "ru";
+    // Change this to switch dual subtitle language
+    const LANG = "en";
 
-    // TODO: use yt-navigate-finish to know when pages finished loading
-
-    // Returns url for Youtube Subtitle API: youtube.com/api/timedtext...
+    // Returns url for YouTube Subtitle API: youtube.com/api/timedtext...
     // including PO Token, without which fetching subtitle returns empty string. Should look something like
     // pot=<PO_TOKEN>&fmt=json3&xorb=2&xobt=3&xovt=3&cbr=Firefox&cplatform=DESKTOP
     // See https://github.com/yt-dlp/yt-dlp/wiki/PO-Token-Guide for more info
@@ -43,7 +42,9 @@
 
             if (isTimedText && hasPot) {
                 console.log("Found matching timedtext request with &pot= parameter!");
-                timedTextUrl = entry.name;
+                timedTextUrl = new URL(entry.name);
+                timedTextUrl.searchParams.set("fmt", "vtt"); // track elems require vtt format
+                timedTextUrl.searchParams.delete("tlang"); // tlang links are for auto-translated subs
                 break;
             }
         }
@@ -51,10 +52,51 @@
         if (!timedTextUrl) {
             throw new Error("No timedtext URL / requests with &pot= parameter found");
         }
-
+        console.log("Timed text url: " + timedTextUrl);
         return timedTextUrl;
     }
+
+    // Returns subtitle data in URI form + vtt format, to use in track element
+    async function fetchSubtitleData(url) {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Couldn't fetch subtitle data");
+        }
+
+        const responseText = await response.text();
+        return "data:text/vtt," + encodeURIComponent(responseText);
+    }
+
+    // Add track element to video given subtitle data
+    function addSubtitle(subData){
+        console.log("Adding subtitle to video");
+        const video = document.querySelector("video");
+        const track = document.createElement("track");
+        track.src = subData;
+        track.track.mode = "showing";
+        video.appendChild(track);
+    }
+
     function pause (milliseconds) {
         return new Promise((resolve) => setTimeout(resolve, milliseconds));
     }
+
+    async function main(){
+        try {
+            let baseTimedTextUrl = await extractTimedTextUrl();
+            let dualSubUrl = new URL(baseTimedTextUrl);
+            dualSubUrl.searchParams.set("lang", LANG);
+
+            let dualSubData = await fetchSubtitleData(dualSubUrl);
+            addSubtitle(dualSubData);
+        } catch(e) {
+            console.error(e);
+        }
+
+    }
+
+    document.addEventListener("yt-navigate-finish", () => {
+        setTimeout(main, 5000)
+    });
 })();
